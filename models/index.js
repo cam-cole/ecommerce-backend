37d751c93a30c6e8a367c10adc40f50a97096bb4
cam-cole <camcole065@gmail.com>
2022-07-18 23:38:55 -0500

    finished api routes

--- models/index.js
+++ models/index.js
@@ -15,13 +15,13 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'tags',
+  as: 'product',
   foreignKey: 'product_id'
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'tags',
+  as: 'tag',
   foreignKey: 'tag_id'
 })
 module.exports = {
