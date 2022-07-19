fbcb5e25c9d0c6e0d3c00fccaba43f26b2136b72
cam-cole <camcole065@gmail.com>
2022-07-18 19:16:01 -0500

    finished models

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,38 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.Integer,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.String,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.Decimal,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.Integer,
+      allowNull: false,
+      default: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.Integer,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
